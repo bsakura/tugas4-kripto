2ad4c6db9e6610cd04be7a878ca697118354bb29
bsakura <13518066@std.stei.itb.ac.id>
2021-11-03 21:21:02 +0700

    Update elgamal.py

--- elgamal.py
+++ elgamal.py
@@ -55,3 +55,18 @@ def decrypt(ciphertext, p, key, q):
 	return plaintext
 
 
+msg = 'encryption'
+print("Original Message :", msg)
+
+q = random.randint(pow(10, 20), pow(10, 50))
+g = random.randint(2, q)
+
+key = generate_key(q)
+h = power(g, key, q)
+print("g used : ", g)
+print("g^a used : ", h)
+
+en_msg, p = encrypt(msg, q, h, g)
+dr_msg = decrypt(en_msg, p, key, q)
+dmsg = ''.join(dr_msg)
+print("Decrypted Message :", dmsg);
